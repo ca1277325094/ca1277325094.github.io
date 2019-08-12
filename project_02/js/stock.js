4324ab86d1dd47042f1de212c2f9564a6c3b4cc2
caochao <1277325094@qq.com>
2019-08-12 08:59:17 +0800

    添加了project_02

---
+++ project_02/js/stock.js
@@ -0,0 +1,264 @@
+var stock={
+	"showapi_res_error": "",
+	"showapi_res_id": "976d65198f134a47933120c54328ea34",
+	"showapi_res_code": 0,
+	"showapi_res_body": {
+	  "ret_code": 0,
+	  "month": "201703",
+	  "list": [
+		{
+		  "date": "2017-03-31",
+		  "open_price": "3206.25",
+		  "max_price": "3226.25",
+		  "min_price": "3205.54",
+		  "close_price": "3222.51",
+		  "diff_money": "12.28",
+		  "diff_rate": "0.38",
+		  "trade_num": "196442922",
+		  "trade_money": "214036159962"
+		},
+		{
+		  "date": "2017-03-30",
+		  "open_price": "3235.14",
+		  "max_price": "3240.02",
+		  "min_price": "3195.85",
+		  "close_price": "3210.24",
+		  "diff_money": "-31.08",
+		  "diff_rate": "-0.96",
+		  "trade_num": "247135479",
+		  "trade_money": "265694871643"
+		},
+		{
+		  "date": "2017-03-29",
+		  "open_price": "3252.87",
+		  "max_price": "3262.10",
+		  "min_price": "3233.28",
+		  "close_price": "3241.31",
+		  "diff_money": "-11.63",
+		  "diff_rate": "-0.36",
+		  "trade_num": "216105575",
+		  "trade_money": "245371852620"
+		},
+		{
+		  "date": "2017-03-28",
+		  "open_price": "3265.63",
+		  "max_price": "3265.63",
+		  "min_price": "3246.09",
+		  "close_price": "3252.95",
+		  "diff_money": "-14.01",
+		  "diff_rate": "-0.43",
+		  "trade_num": "161710013",
+		  "trade_money": "203452481771"
+		},
+		{
+		  "date": "2017-03-27",
+		  "open_price": "3268.92",
+		  "max_price": "3283.24",
+		  "min_price": "3262.12",
+		  "close_price": "3266.96",
+		  "diff_money": "-2.49",
+		  "diff_rate": "-0.08",
+		  "trade_num": "201852675",
+		  "trade_money": "249185540341"
+		},
+		{
+		  "date": "2017-03-24",
+		  "open_price": "3247.35",
+		  "max_price": "3275.21",
+		  "min_price": "3241.12",
+		  "close_price": "3269.45",
+		  "diff_money": "20.90",
+		  "diff_rate": "0.64",
+		  "trade_num": "219777914",
+		  "trade_money": "267094164315"
+		},
+		{
+		  "date": "2017-03-23",
+		  "open_price": "3245.81",
+		  "max_price": "3262.09",
+		  "min_price": "3221.93",
+		  "close_price": "3248.55",
+		  "diff_money": "3.33",
+		  "diff_rate": "0.10",
+		  "trade_num": "193029144",
+		  "trade_money": "258246691000"
+		},
+		{
+		  "date": "2017-03-22",
+		  "open_price": "3246.22",
+		  "max_price": "3255.78",
+		  "min_price": "3229.13",
+		  "close_price": "3245.22",
+		  "diff_money": "-16.39",
+		  "diff_rate": "-0.50",
+		  "trade_num": "189731649",
+		  "trade_money": "244545459040"
+		},
+		{
+		  "date": "2017-03-21",
+		  "open_price": "3250.25",
+		  "max_price": "3262.22",
+		  "min_price": "3246.70",
+		  "close_price": "3261.61",
+		  "diff_money": "10.80",
+		  "diff_rate": "0.33",
+		  "trade_num": "162719306",
+		  "trade_money": "219121269781"
+		},
+		{
+		  "date": "2017-03-20",
+		  "open_price": "3241.11",
+		  "max_price": "3251.13",
+		  "min_price": "3228.12",
+		  "close_price": "3250.81",
+		  "diff_money": "13.36",
+		  "diff_rate": "0.41",
+		  "trade_num": "170548430",
+		  "trade_money": "213951759501"
+		},
+		{
+		  "date": "2017-03-17",
+		  "open_price": "3271.87",
+		  "max_price": "3274.19",
+		  "min_price": "3232.28",
+		  "close_price": "3237.45",
+		  "diff_money": "-31.49",
+		  "diff_rate": "-0.96",
+		  "trade_num": "200583223",
+		  "trade_money": "262184046475"
+		},
+		{
+		  "date": "2017-03-16",
+		  "open_price": "3247.16",
+		  "max_price": "3269.77",
+		  "min_price": "3247.16",
+		  "close_price": "3268.94",
+		  "diff_money": "27.18",
+		  "diff_rate": "0.84",
+		  "trade_num": "189416002",
+		  "trade_money": "244508344465"
+		},
+		{
+		  "date": "2017-03-15",
+		  "open_price": "3235.40",
+		  "max_price": "3243.71",
+		  "min_price": "3227.74",
+		  "close_price": "3241.76",
+		  "diff_money": "2.43",
+		  "diff_rate": "0.08",
+		  "trade_num": "144055682",
+		  "trade_money": "185727250363"
+		},
+		{
+		  "date": "2017-03-14",
+		  "open_price": "3235.25",
+		  "max_price": "3246.33",
+		  "min_price": "3231.52",
+		  "close_price": "3239.33",
+		  "diff_money": "2.30",
+		  "diff_rate": "0.07",
+		  "trade_num": "146946022",
+		  "trade_money": "191535126690"
+		},
+		{
+		  "date": "2017-03-13",
+		  "open_price": "3209.45",
+		  "max_price": "3237.12",
+		  "min_price": "3193.16",
+		  "close_price": "3237.02",
+		  "diff_money": "24.26",
+		  "diff_rate": "0.76",
+		  "trade_num": "163673760",
+		  "trade_money": "206919741904"
+		},
+		{
+		  "date": "2017-03-10",
+		  "open_price": "3213.73",
+		  "max_price": "3222.32",
+		  "min_price": "3208.45",
+		  "close_price": "3212.76",
+		  "diff_money": "-3.99",
+		  "diff_rate": "-0.12",
+		  "trade_num": "136672743",
+		  "trade_money": "173663170234"
+		},
+		{
+		  "date": "2017-03-09",
+		  "open_price": "3233.70",
+		  "max_price": "3233.87",
+		  "min_price": "3205.28",
+		  "close_price": "3216.75",
+		  "diff_money": "-23.92",
+		  "diff_rate": "-0.74",
+		  "trade_num": "167371108",
+		  "trade_money": "199217646811"
+		},
+		{
+		  "date": "2017-03-08",
+		  "open_price": "3240.53",
+		  "max_price": "3245.30",
+		  "min_price": "3230.61",
+		  "close_price": "3240.66",
+		  "diff_money": "-1.74",
+		  "diff_rate": "-0.05",
+		  "trade_num": "160731388",
+		  "trade_money": "198225782571"
+		},
+		{
+		  "date": "2017-03-07",
+		  "open_price": "3233.09",
+		  "max_price": "3242.66",
+		  "min_price": "3226.82",
+		  "close_price": "3242.41",
+		  "diff_money": "8.54",
+		  "diff_rate": "0.26",
+		  "trade_num": "164064235",
+		  "trade_money": "209931208364"
+		},
+		{
+		  "date": "2017-03-06",
+		  "open_price": "3217.33",
+		  "max_price": "3234.66",
+		  "min_price": "3215.07",
+		  "close_price": "3233.87",
+		  "diff_money": "15.55",
+		  "diff_rate": "0.48",
+		  "trade_num": "156092158",
+		  "trade_money": "193769172395"
+		},
+		{
+		  "date": "2017-03-03",
+		  "open_price": "3219.20",
+		  "max_price": "3221.16",
+		  "min_price": "3206.61",
+		  "close_price": "3218.31",
+		  "diff_money": "-11.72",
+		  "diff_rate": "-0.36",
+		  "trade_num": "157082368",
+		  "trade_money": "192707923011"
+		},
+		{
+		  "date": "2017-03-02",
+		  "open_price": "3250.52",
+		  "max_price": "3256.81",
+		  "min_price": "3228.66",
+		  "close_price": "3230.03",
+		  "diff_money": "-16.91",
+		  "diff_rate": "-0.52",
+		  "trade_num": "181215076",
+		  "trade_money": "223043958766"
+		},
+		{
+		  "date": "2017-03-01",
+		  "open_price": "3240.07",
+		  "max_price": "3259.98",
+		  "min_price": "3237.87",
+		  "close_price": "3246.93",
+		  "diff_money": "5.20",
+		  "diff_rate": "0.16",
+		  "trade_num": "190677550",
+		  "trade_money": "225940967941"
+		}
+	  ]
+	}
+  }
